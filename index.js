308c7187801e09cc02544e11164842aac90fb6b0
Max Schmeling <schmeling88@gmail.com>
2018-01-30 17:16:05 -0600

    Use pagePool.

--- index.js
+++ index.js
@@ -3,63 +3,71 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const Queue = require('promise-queue');
 const fs = require('fs');
+const genericPool = require('generic-pool');
 
 const frameMessage = (frame, frames) =>
   `[puppeteer-recorder] rendering frame ${frame} of ${frames}.`;
 
-async function processWithPage(browser, page, pageIndex, pageCount, options) {
-  const maxConcurrent = 5;
-  const maxQueue = Infinity;
-  const queue = new Queue(maxConcurrent, maxQueue);
-  const writePromises = [];
+async function processWithPage(pagePool, frame, options) {
+  // const maxConcurrent = 5;
+  // const maxQueue = Infinity;
+  // const queue = new Queue(maxConcurrent, maxQueue);
+  // const writePromises = [];
+  const page = await pagePool.acquire();
 
-  for (let i = 1; i <= options.frames; i += pageCount) {
-    if (i + pageIndex > options.frames) return;
     if (options.logEachFrame)
-      console.log(frameMessage(i + pageIndex, options.frames));
+      console.log(frameMessage(frame, options.frames));
 
-    await options.render(browser, page, i + pageIndex);
+    await options.render(page, frame);
 
     const outputPath = path.join(
       options.dir,
       `img${('0000' + (i + pageIndex)).substr(-4, 4)}.png`
     );
 
-    let writeProm;
-
     if (options.screenshot)
       await options.screenshot(async () => {
-        const bfr = await page.screenshot();
-        writePromises.push(
-          queue.add(() => {
-            return new Promise((resolve, reject) => {
-              try {
-                fs.writeFileSync(outputPath, bfr);
-                resolve();
-              } catch (err) {
-                reject(err);
-              }
-            });
-          })
-        );
+        // const bfr = await page.screenshot({ path: outputPath });
+        await page.screenshot({ path: outputPath });
+        // writePromises.push(
+        //   queue.add(() => {
+        //     return new Promise((resolve, reject) => {
+        //       try {
+        //         fs.writeFileSync(outputPath, bfr);
+        //         resolve();
+        //       } catch (err) {
+        //         reject(err);
+        //       }
+        //     });
+        //   })
+        // );
       });
     else await page.screenshot({ path: outputPath });
   }
 
-  await Promise.all(writePromises);
+  await pagePool.releaes(page);
+
+  // await Promise.all(writePromises);
 }
 
 module.exports.record = async function record(options) {
   const pageCount = options.pageCount || 1;
 
-  const browsers = options.browsers;
-  const pagePromises = [];
-  for (let i = 0; i < pageCount; i++) {
-    pagePromises.push(browsers[i].newPage());
-  }
-  const pages = await Promise.all(pagePromises);
-
-  await Promise.all(pages.map((p, i) => options.prepare(browsers[i], p)));
+  const { browserPool } = options;
+  const pagePool = genericPool.createPool(
+    {
+      create: async () => {
+        const browser = await browserPool.acquire();
+        const page = await browser.newPage();
+        await options.prepare(browser, page);
+      },
+      destroy: async page => {
+        await page.close();
+        await browserPool.release(browser);
+      }
+    },
+    { max: pageCount }
+  );
 
   var ffmpegPath = options.ffmpeg || 'ffmpeg';
   var fps = options.fps || 60;
@@ -75,17 +83,26 @@ module.exports.record = async function record(options) {
 
   args.push(outFile || '-');
 
-  await Promise.all(
-    pages.map((page, pageIndex) =>
-      processWithPage(
-        browsers[pageIndex],
-        page,
-        pageIndex,
-        pages.length,
-        options
-      )
-    )
-  );
+  const prom = [];
+
+  for (let i = 1; i <= options.frames; i++) {
+    prom.push(processWithPage(pagePool, i, options))  
+  }
+
+  await Promise.all(prom);
+
+  const drainPromise = pagePool.drain();
+
+  // await Promise.all(
+  //   pages.map((page, pageIndex) =>
+  //     processWithPage(
+  //       page,
+  //       pageIndex,
+  //       pages.length,
+  //       options
+  //     )
+  //   )
+  // );
 
   const ffmpeg = spawn(ffmpegPath, args);
 
@@ -100,7 +117,7 @@ module.exports.record = async function record(options) {
   });
 
   await closed;
-  await Promise.all(pages.map(p => p.close()));
+  await drainPromise;
 };
 
 const ffmpegArgs = (fps, originalPath, threadQueueSize, dir) => {
